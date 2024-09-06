fdd02f9cb64698d6ab14e4bfccb2d9bcdb47bebb
Ben Tupper <btupper@bigelow.org>
2024-09-06 11:37:19 -0400

    initial meaningful commit

---
+++ R/catalog.R
@@ -0,0 +1,36 @@
+#' Retrieve a catalog based upon region and period
+#' 
+#' @export
+#' @param period chr one of "history" or "forecast"
+#' @param region chr, one of "Northwest Atlantic", "NWA", "Northeast Pacific" or "NEP"
+#' @return character URL
+catalog_uri = function(period = c("history", "forecast")[1],
+                       region = c("NWA", "NEP")[1]){
+  
+  # https://psl.noaa.gov/cefi_portal/var_list_northwest_atlantic_hist_run.json
+  # https://psl.noaa.gov/cefi_portal/var_list_northwest_atlantic_forecast.json
+  
+  reg = switch(tolower(region[1]),
+    "northwest atlantic" = "northwest_atlantic", 
+    "nwa" = "northwest_atlantic", 
+    "northeast_pacific")
+  per = switch(tolower(period[1]),
+    "history" = "hist_run",
+    "historcial" = "hist_run",
+    "forecast")
+
+  sprintf("https://psl.noaa.gov/cefi_portal/var_list_%s_%s.json", reg, per)
+}
+
+
+#' Read the CEFI catalog
+#' 
+#' @export
+#' @param uri chr, the URI of the json resource
+#' @return table of metadata
+read_catalog = function(uri = catalog_uri(region = "NWA", period = "history")){
+  
+  jsonlite::read_json(uri, simplifyVector= TRUE) |>
+    lapply(dplyr::as_tibble) |>
+    dplyr::bind_rows()
+}
---
+++ R/cefi-package.R
@@ -0,0 +1,7 @@
+#' cefi package
+#'
+#' @name cefi
+#' @docType package
+#' @description R Tools for accessing the NOAA/PSL Climate Ecosystems and Fisheries Initiative Portal. 
+#' @importFrom rlang .data 
+"_PACKAGE"
